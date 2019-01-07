830bb3ca1ee30abec108e75c4c492c552144299e
Shanmugasundaram Manickam <shanmugasundarammanickam@Shanmugasundarams-MacBook-Air.local>
2019-01-07 16:58:28 -0600

    added basic html content & function to select random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,18 @@
-console.log($(document));
+//As the page loads, reate a random number between 19 and 120 and display it on the page
+$(window).on('load', function() {
+    //random number between 19-120
+    var random = Math.floor(Math.random() * 120 + 1);
+    console.log('Random Number: ' + random);
+    $('#randomNumber').text(random);
+})
+
+
+
+
+
+
+
+
+
+
+
