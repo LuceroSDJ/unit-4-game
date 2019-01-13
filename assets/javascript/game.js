82dc275e91f9fd4f75a16e1867af894afb3f99fa
Shanmugasundaram Manickam <shanmugasundarammanickam@Shanmugasundarams-MacBook-Air.local>
2019-01-12 23:54:42 -0600

    updated files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,35 +4,63 @@ var losses = 0;
 var sumTotal = 0;
 //var sumTotal = [];
 var random = 0;
-//Then, when user clicks on the crystal, add the value of the clicked crystal to the empty array or as a string to evaluate later. Explore which option is better?
+var hiddenValues = [''];
 
+//array: crystal images 
+var images = ['assets/images/crystal1.gif','assets/images/crystal5.gif','assets/images/crystal10.gif', 'assets/images/crystal20.gif'];
 
-//Initialize crystal variables with a random number between 1 and 12
-var crystalOne = Math.floor(Math.random() * 11 + 1);
-var crystalTwo = Math.floor(Math.random() * 11 + 1);
-var crystalThree = Math.floor(Math.random() * 11 + 1);
-var crystalFour = Math.floor(Math.random() * 11 + 1);
+//for loop(Initialize crystal variables with a random number between 1 and 12)
+function createHiddenValues() {
+    for(i=0; i < 4; i++) {
+        //push hiden value inside hidenValues array
+        hiddenValues.push(Math.floor(Math.random() * 11 + 1));
+        console.log('crystal hidden values: ' + hiddenValues);
+    }
+}
+
+//Generate crystals
+function generateCrystals() {
+    createHiddenValues();
+    //add crystal images to empty div
+    for(var i=0; i<hiddenValues.length; i++) {
+        var crystal = $('<img>');
+        crystal.addClass('crystalImg');
+        crystal.attr('value', hiddenValues[i]);
+        crystal.attr('src', images[i]);
+        $('.crystalsDiv').append(crystal);
+    }
+}
 
 //As the page loads, create a random number between 19 and 120 and display it on the page
 $(window).on('load', function() {
+    generateCrystals();
     //random number between 19-120
     random = Math.floor(Math.random() * 101) + 19;
     console.log('Random Number: ' + random);
     $('#randomNumber').text(random);
-    //return random; test it 
+   
 });
 
-//****************************** 
-//create reset function to recall in future functions
-//******************************
-                    //convert sumTotal into a real operation (strings to integers and operators)
+//When user clicks on crystal 1, a random hidden values must be added to the next & previous hidden values
+$('.crystalsDiv').on('click', function() {
+    console.log(hiddenValues[i]);
+    /*add hidden value to var sumTotal
+    shorthand operator of sumTotal = sumTotal + crystalOne;*/
+    sumTotal += hiddenValues[i];
+    console.log('your total so far: ' + sumTotal);
+    //print 'score so far' in the html empty tag 
+    $('#totalScore').text(sumTotal);
+    //conditions();
+});
 
 //Define conditions. When user wins or loses, increment points by 1 accordingly
 function conditions() {
     if(random === sumTotal) {
         //increment wins
         wins++;
-        //generate a different random number between 19 and 120
+        //print wins(+1 point) on the page & by adding comma function ()... this function will waint until our previous lines of code are executed
+        $('.wins').text(wins, (function() {
+        //Now, generate a different random number between 19 and 120
         random = Math.floor(Math.random() * 101) + 19;
         //test
         console.log('Random Number: ' + random);
@@ -49,17 +77,14 @@ function conditions() {
         console.log('reset' + sumTotal);
         //print sumTotal=0 in the page
         $('#totalScore').text(sumTotal);
-        //print score on the page
-        $('.wins').text(wins);
-        //****************************************************** 
-        //congratulate user
-        alert('You win!');
-        /*right now my alert is preventing the user from seeing the actual FINAL total amount.
-        The alert box pops up before the final score is displayed and resets my totalSum back to zero right away*/
-        //******************************************************
+        alert('you win');
+        }));
+       
     }else if(random < sumTotal) {
         //increment losses
         losses++;
+        //print incremented losses on the page
+        $('.losses').text(losses, $(function() {
         //generate a different random number between 19 and 120
         random = Math.floor(Math.random() * 101) + 19;
         //test
@@ -75,85 +100,43 @@ function conditions() {
         sumTotal = 0;
         //print sumTotal=0 in the page
         $('#totalScore').text(sumTotal);
-        //print score on the page
-        $('.losses').text(losses);
-        alert('I win!!!!!!!');
-
+        alert('Good luck next time!!!!!!!');
+        }));
     }
-}
 
 
-//When user clicks on crystal 1, a random hidden values must be added to the next & previous hidden values
-$('.firstCrystal').on('click', function() {
-    /************************************************** 
-    A new hidden value is generated.......now, it is generated in our if/else statement when user wins or loses. Is this what we want???
-    crystalOne; also, there is no need to recall my variable here since my variable is already defined in the global scope
-    //**************************************************/
-    console.log(crystalOne);
-    //add hidden value to var sumTotal. 
-    sumTotal = sumTotal + crystalOne;
-    console.log('your total so far: ' + sumTotal);
+
+
+
+
+/*
+$('.crystalsDiv').on('click', function() {
+    //console.log the hidden value of the clicked crystal
+    var value = ($(this).attr('value'));
+    console.log($(this).attr('value'));
+    //console.log($(this.value));
+
     //convert string into integer
     //sumTotal = parseInt(sumTotal);
-    //print 'score so far' in the html empty tag
-    $('#totalScore').text(sumTotal);
+    /*add hidden value to var sumTotal
+    shorthand operator of sumTotal = sumTotal + crystalOne;*/
+    //sumTotal += hiddenValues[i];
+    //console.log('your total so far: ' + sumTotal);
+    
+    //console.log('this: ' + $(this));
+    
+    
+
+    /*add hidden value to var sumTotal
+    shorthand operator of sumTotal = sumTotal + crystalOne;*/
+    ////////////sumTotal += value;
+    //////////console.log('your total so far: ' + sumTotal);
+    //convert string into integer
+    //sumTotal = parseInt(sumTotal);
+    //print 'score so far' in the html empty tag and add call back function
+    //////////$('#totalScore').text(sumTotal, $(function(){
     //recall 'retur function conditions'
-    conditions();
-});
-
-//When user clicks on crystal 2, a random hidden values must be added to the next & previous hidden values
-$('.secondCrystal').on('click', function() {
-    console.log(crystalTwo);
-    //add hidden value to var sumTotal. 
-    sumTotal = sumTotal + crystalTwo;
-    //test
-    console.log('your total so far: ' + sumTotal);
-    //convert sumTotal from string into integer
-    //sumTotal = parseInt(sumTotal);
-    //print 'score so far' in the html empty tag
-    $('#totalScore').text(sumTotal);
-    conditions();
-});
-
-//When user clicks on crystal 3, a random hidden values must be added to the next & previous hidden values
-$('.thirdCrystal').on('click', function() {
-    console.log(crystalThree);
-    //add our hidden value to var sumTotal. 
-    sumTotal = sumTotal + crystalThree;
-    //test
-    console.log('your total so far: ' + sumTotal);
-    //convert sumTotal from string into integer
-    //sumTotal = parseInt(sumTotal);
-    //print 'score so far' in the html empty tag
-    $('#totalScore').text(sumTotal);
-    conditions();
-});
-
-//When user clicks on crystal 4, a random hidden values must be added to the next & previous hidden values
-$('.fourthCrystal').on('click', function() {
-    console.log(crystalFour);
-    //add our hidden value to var sumTotal. 
-    sumTotal = sumTotal + crystalFour;
-    //test
-    console.log('your total so far: ' + sumTotal);
-    //convert sumTotal from string into integer
-    //sumTotal = parseInt(sumTotal);
-    //print 'score so far' in the html empty div
-    $('#totalScore').text(sumTotal);
-    conditions();
-});
-
-
-
-///////when clicking the crystal, the hidden value must be added to the current sum(previous sumSubTotal) until it matches the random number or it goes over 
-///////when clicking the same crystal(same game), the crystal should have the same hidden value
-
-//if sum is egual to random number, increment wins & start game over
-//else increment losses
-
-//restart game without lossing the score
-
-//also, I want to add a prompt window to cancel the game
-//add an alert "congratulations" if user wins
-//reset button????
-
+    //conditions();
+    //$('.firstCrystal').slideToggle(200); //wow! se desaparece*/
+    
+//});
